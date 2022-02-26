434783750ea4b85b2408d4202c6053fcd33ac71c
Daniel G <dgordon19990@gmail.com>
2022-02-25 19:04:23 -0600

    CopiestPasta

---
+++ CacheMoney/backend/src/main/java/com/revature/cachemoney/backend/beans/controllers/UserController.java
@@ -0,0 +1,87 @@
+package com.revature.cachemoney.backend.beans.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.revature.cachemoney.backend.beans.models.User;
+import com.revature.cachemoney.backend.beans.repositories.UserRepo;
+
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Optional;
+
+import javax.sql.DataSource;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	private final UserRepo userRepository;
+
+    @Autowired
+    public UserController(UserRepo userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // GET all users
+    @RequestMapping(method = RequestMethod.GET)
+    public List<User> getAllUsers(){
+       return userRepository.findAll();
+    }
+
+    // GET a user by ID
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<User> getUserById(@PathVariable Integer id){
+
+        return userRepository.findById(id);
+    }
+
+    /**
+     * POST a User.
+     * 
+     * @param user
+     * @return String containing information regarding success or failure.
+     */
+    @PostMapping()
+    public String postUser(@RequestBody User user) {
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            System.out.println("User cannot be registered.");
+
+            // inform failed result
+            return "User cannot be registered. The email you entered ("
+                    + user.getEmail() + ") may already be in use.";
+        }
+
+        // inform successful result
+        return "User " + user.getFirstName() + user.getLastName() +
+                " (ID = " + user.getUser_id() +
+                ") registered successfully!";
+    }
+
+    // DELETE a user by ID
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteUserById(@PathVariable Integer id){
+        userRepository.deleteById(id);
+    }
+    
+    /**
+     * GET a User by email.
+     * Emails are unique and should not cause conflicting results.
+     * 
+     * @param email
+     * @return the User based on email
+     */
+    @GetMapping(value = "/email")
+    public Optional<User> getUserByEmail(@RequestParam String email) {
+    	System.out.println(email + "<<<<<");
+        return userRepository.findByEmail(email);
+    }
+    
+    
+}
---
+++ CacheMoney/backend/src/main/java/com/revature/cachemoney/backend/bones/AppUtils.java
@@ -0,0 +1,52 @@
+package com.revature.cachemoney.backend.bones;
+
+import java.io.IOException;
+
+public class AppUtils {
+
+	public static void Log() {
+		System.out.println("");
+	}
+
+	public static void Log(String l) {
+		System.out.println(l);
+	}
+
+	public static void Log(Object o) {
+		if (o == null)
+			System.out.println(o);
+		else
+			System.out.println(o.toString());
+	}
+
+	public static void Page() {
+		for (int i = 0; i < 8; i++) {
+			Log();
+		}
+	}
+
+	public static void Clear() {
+		Page();
+		Page();
+	}
+
+	//////
+	// prints stack trace with message
+	public static void ThrowFancyException(String messOut) {
+		ThrowFancyException(messOut, false);
+	}
+
+	public static void ThrowFancyException(String messOut, boolean breakIt) {
+		for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+			Log(ste);
+		}
+		Log(messOut);
+
+		if (breakIt) {
+			System.exit(0);
+			int f = 1 / 0; // just in case lol
+		}
+	}
+	//
+	//////
+}
