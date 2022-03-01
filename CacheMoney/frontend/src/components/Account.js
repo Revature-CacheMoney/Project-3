f11a92efd4c835988d0772e6b7555e49d10577b2
Cheryl See <cheryl.see@revature.net>
2022-03-01 10:08:38 -0600

    Latest stuff

--- CacheMoney/frontend/src/components/Account.js
+++ CacheMoney/frontend/src/components/Account.js
@@ -3,7 +3,7 @@ import React from "react";
 // The Account component should hold multiple transactions related to this account.
 // Also may contain other related account info (name, account number) and possibly link to do
 // transfers as well (or another parent component?)
-function Account() {
+function Account(props) {
 	return <></>;
 }
 
