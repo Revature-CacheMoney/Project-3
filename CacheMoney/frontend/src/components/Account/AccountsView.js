f4592880fd0f22bb74770b98e182acac6e054f3d
jondgons <jondgons@gmail.com>
2022-03-02 16:36:28 -0600

    starting accounts view stuff

--- CacheMoney/frontend/src/components/Account/AccountsView.js
+++ CacheMoney/frontend/src/components/Account/AccountsView.js
@@ -1,17 +1,18 @@
-import React from "react";
-import Navigation from "../NavBar";
+import React, { useState } from "react";
 
 // The Account component should hold multiple transactions related to this account.
 // Also may contain other related account info (name, account number) and possibly link to do
 // transfers as well (or another parent component?)
 function AccountsView(props) {
+    // local transaction state
+    const [accounts, getAccounts] = useState([]);
+
+    // url
+    const url = "http://localhost:8080/";
+
 	return (
 		<>
-		<Navigation />
-			<div id="account-container">
-				<h3>Account Header</h3>
-				<p>Diffirent accounts</p>
-			</div>
+			
 		</>
 	);
 }
