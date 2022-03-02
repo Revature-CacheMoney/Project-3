af214513b357553bcc4ad465f4505f70831e4e15
Daniel G <dgordon19990@gmail.com>
2022-03-02 14:06:33 -0600

    1

--- CacheMoney/frontend/src/App.js
+++ CacheMoney/frontend/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import "./css/App.css";
+import Splash from "./components/Splash.js";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Signin from "./components/Signin.js";
+import Register from "./components/Register.js";
+import NotFound from "./components/NotFound.js";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/" exact element={<Splash />} />
+					<Route path="/signin" exact element={<Signin />} />
+					<Route path="/register" exact element={<Register />} />
+					<Route element={<NotFound />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
