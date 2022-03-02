d50a69f51fa62bbcd43c7150e735f654be7dd6d3
Daniel G <dgordon19990@gmail.com>
2022-03-02 13:24:54 -0600

    current-year state of the art

--- CacheMoney/backend/src/main/java/com/revature/cachemoney/backend/beans/repositories/TransactionRepo.java
+++ CacheMoney/backend/src/main/java/com/revature/cachemoney/backend/beans/repositories/TransactionRepo.java
@@ -1,5 +1,8 @@
 package com.revature.cachemoney.backend.beans.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,6 @@ import com.revature.cachemoney.backend.beans.models.Transaction;
 @Repository
 public interface TransactionRepo extends JpaRepository<Transaction, Integer> {
 
+	List<Transaction> findByAccountId(Integer acctId);
+	
 }
