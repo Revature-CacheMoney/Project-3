223f191fc5ff56cf3bfa2d6028f190c72b79098b
jondgons <jondgons@gmail.com>
2022-03-07 11:57:07 -0600

    basic account creation

--- CacheMoney/frontend/src/components/Account/CreateAccount.js
+++ CacheMoney/frontend/src/components/Account/CreateAccount.js
@@ -1,39 +1,52 @@
 import axios from "axios";
+import { useState } from "react";
 import config from "../../config";
+import store from "../../store/Store.js";
 
 function CreateAccount(props) {
+    const [formData, setFormData] = useState({
+        name: "",
+        balance: 0,
+        type: "",
+        userId: {
+            // store.getState().userId
+            user_id: 1
+        }
+    })
+
     // retrieve the url from the config
     const url = config.url;
 
     // post account
     const postAccount = (account) => {
         axios.post(`${url}accounts`, account)
-            .then((response) => {
-                // TODO delete this
-                console.log(account);
-            })
             .catch(error => console.error(`Error: ${error}`));
+    }
+
+    // updates form data when form changes
+    const handleChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
     }
 
     // what the submit button should do
     const handleSubmit = () => {
-
+        postAccount(formData);
     }
 
     return (
         <>
             <div>
                 <form>
-                    <label htmlFor="name">Account name: </label>
-                    <input type="text" id="name" />
+                    <label htmlFor="account_name">Account name: </label>
+                    <input type="text" id="account_name" name="name" onChange={handleChange} />
 
-                    <input type="radio" id="checking" name="account_type" value="Checking" />
+                    <input type="radio" id="checking" name="type" value="checking" onChange={handleChange} />
                     <label htmlFor="checking">Checking</label>
 
-                    <input type="radio" id="savings" name="account_type" value="Savings" />
+                    <input type="radio" id="savings" name="type" value="savings" onChange={handleChange} />
                     <label htmlFor="savings">Savings</label>
 
-                    <button name="submit" onClick={handleSubmit}>Submit</button>
+                    <button type="button" name="submit" onClick={handleSubmit}>Submit</button>
                 </form>
             </div>
         </>
