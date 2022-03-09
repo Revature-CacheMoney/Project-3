06336ca4c19f1b7fb7a8736ebe5027de0314a2b4
Donald Farrar <donfar09@gmail.com>
2022-03-09 13:39:28 -0800

    something happening, I am working backwards on the RegiserView page. Starting with just getting state to update as input is getting registered. Now need to work with it to function correctly with the api call to store the data

--- CacheMoney/frontend/src/components/RegisterView.js
+++ CacheMoney/frontend/src/components/RegisterView.js
@@ -10,127 +10,130 @@ import store from "../store/Store.js";
 function RegisterView() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    name: "",
-    balance: 0,
-    type: "",
-    userId: {
-      user_id: store.getState().userId,
-    },
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: { username: store.getState().userName },
+    password1: "",
+    password2: "",
   });
 
   const handleChange = (event) => {
-	event.preventDefault();
-	setFormData({ ...formData, [event.target.name]: event.target.value });
-}
-
-//   // handleSubmit: What happens when user presses the "submit" button on reg. form
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-
-//     // validate password (do they match?)
-//     const p1 = document.getElementById("password1").value;
-//     const p2 = document.getElementById("password2").value;
-
-//     if (p1 === p2) {
-//       const info = {
-//         firstName: document.getElementById("firstname").value,
-//         lastName: document.getElementById("lastname").value,
-//         email: document.getElementById("email").value,
-//         username: document.getElementById("username").value,
-//         password: document.getElementById("password1").value,
-//       };
-//       // Validate information
-//       if (validateInput(info)) {
-//         // submit stuff
-//         console.log("Information being sent:  ", info);
-//         doRegistration(info);
-//       }
-//     } else {
-//       alert("Sorry, your passwords do dnot match.");
-//     }
-//   };
-
-  // Checks input against validation (should be same patterns as used in backend)
-  function validateInput(info) {
-    const { firstName, lastName, email, username, password } = info;
-
-    // Test the firstname, lastname for validity - ex. no empty strings
-    const namePattern = /^[a-zA-Z -]+$/;
-    if (!namePattern.test(firstName)) {
-      alert("Invalid first name.  Please check the spelling and try again.");
-      return false;
-    } else if (!namePattern.test(lastName)) {
-      alert("Invalid last name.  Please check the spelling and try again.");
-      return false;
-    }
-
-    const emailPattern =
-      /^[a-zA-Z0-9._-]+@{1}[a-zA-Z0-9-_]+[.]{1}[a-zA-Z0-9]+[a-zA-Z_.-]*$/;
-    if (!emailPattern.test(email)) {
-      alert("Invalid email address.  Please check your input and try again.");
-      return false;
-    }
-
-    const usernamePattern = /^[a-zA-Z0-9@~._-]{8,}$/;
-    if (!usernamePattern.test(username)) {
-      alert(
-        "Invalid username.  Usernames should be between 8-255 characters in length and use alphanumeric / select symbols.."
-      );
-      return false;
-    }
-
-    const passwordPattern = /^[a-zA-Z0-9@^%$#/\\,;|~._-]{8,50}$/;
-    if (!passwordPattern.test(password)) {
-      alert(
-        "Invalid password.  Passwords should be between 8-50 characters in length and use alphanumeric / select symbols.."
-      );
-      return false;
-    }
-
-    return true;
-  }
-
-  // doRegistration: does the actual registration call
-  function doRegistration(info) {
-    let responseStatus;
-    // Lets cover any sort of backend response
-    let responseData;
-    const url = config.url;
-
-    axios
-      .post(`${url}users/`, info)
-      .then((response) => {
-        console.log(response);
-        responseStatus = response.status;
-        responseData = response.data;
-        if (responseStatus === 200) {
-          if (responseData === true || responseData.result === true) {
-            console.log("Registration successful");
-            doLoginToMain();
-          } else {
-            registrationFailure();
-          }
-        } else {
-          registrationFailure();
-        }
-      })
-      .catch((error) => console.error(`Error: ${error}`));
-  }
-
-  // Probably want to expand on this to include exactly what was wrong,
-  // use non-alert messaging to highlight errors, etc
-  function registrationFailure() {
-    console.log("Some error occurred during registration.");
-    console.log("Check if the email or username is already in use?");
-    alert(
-      "Some error occurred during registration.  Email or username already in use?"
-    );
-  }
-
-  // TODO: this should route back to index page with note to login
-  const doLoginToMain = () => {
-    navigate("/signin");
+    event.preventDefault();
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+    console.log("formData", formData);
   };
+
+  console.log("handleChange", formData);
+
+  //   // handleSubmit: What happens when user presses the "submit" button on reg. form
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+
+  //     // validate password (do they match?)
+  //     const p1 = document.getElementById("password1").value;
+  //     const p2 = document.getElementById("password2").value;
+
+  //     if (p1 === p2) {
+  //       const info = {
+  //         firstName: document.getElementById("firstname").value,
+  //         lastName: document.getElementById("lastname").value,
+  //         email: document.getElementById("email").value,
+  //         username: document.getElementById("username").value,
+  //         password: document.getElementById("password1").value,
+  //       };
+  //       // Validate information
+  //       if (validateInput(info)) {
+  //         // submit stuff
+  //         console.log("Information being sent:  ", info);
+  //         doRegistration(info);
+  //       }
+  //     } else {
+  //       alert("Sorry, your passwords do dnot match.");
+  //     }
+  //   };
+
+  //   // Checks input against validation (should be same patterns as used in backend)
+  //   function validateInput(info) {
+  //     const { firstName, lastName, email, username, password } = info;
+
+  //     // Test the firstname, lastname for validity - ex. no empty strings
+  //     const namePattern = /^[a-zA-Z -]+$/;
+  //     if (!namePattern.test(firstName)) {
+  //       alert("Invalid first name.  Please check the spelling and try again.");
+  //       return false;
+  //     } else if (!namePattern.test(lastName)) {
+  //       alert("Invalid last name.  Please check the spelling and try again.");
+  //       return false;
+  //     }
+
+  //     const emailPattern =
+  //       /^[a-zA-Z0-9._-]+@{1}[a-zA-Z0-9-_]+[.]{1}[a-zA-Z0-9]+[a-zA-Z_.-]*$/;
+  //     if (!emailPattern.test(email)) {
+  //       alert("Invalid email address.  Please check your input and try again.");
+  //       return false;
+  //     }
+
+  //     const usernamePattern = /^[a-zA-Z0-9@~._-]{8,}$/;
+  //     if (!usernamePattern.test(username)) {
+  //       alert(
+  //         "Invalid username.  Usernames should be between 8-255 characters in length and use alphanumeric / select symbols.."
+  //       );
+  //       return false;
+  //     }
+
+  //     const passwordPattern = /^[a-zA-Z0-9@^%$#/\\,;|~._-]{8,50}$/;
+  //     if (!passwordPattern.test(password)) {
+  //       alert(
+  //         "Invalid password.  Passwords should be between 8-50 characters in length and use alphanumeric / select symbols.."
+  //       );
+  //       return false;
+  //     }
+
+  //     return true;
+  //   }
+
+  //   // doRegistration: does the actual registration call
+  //   function doRegistration(info) {
+  //     let responseStatus;
+  //     // Lets cover any sort of backend response
+  //     let responseData;
+  //     const url = config.url;
+
+  //     axios
+  //       .post(`${url}users/`, info)
+  //       .then((response) => {
+  //         console.log(response);
+  //         responseStatus = response.status;
+  //         responseData = response.data;
+  //         if (responseStatus === 200) {
+  //           if (responseData === true || responseData.result === true) {
+  //             console.log("Registration successful");
+  //             doLoginToMain();
+  //           } else {
+  //             registrationFailure();
+  //           }
+  //         } else {
+  //           registrationFailure();
+  //         }
+  //       })
+  //       .catch((error) => console.error(`Error: ${error}`));
+  //   }
+
+  //   // Probably want to expand on this to include exactly what was wrong,
+  //   // use non-alert messaging to highlight errors, etc
+  //   function registrationFailure() {
+  //     console.log("Some error occurred during registration.");
+  //     console.log("Check if the email or username is already in use?");
+  //     alert(
+  //       "Some error occurred during registration.  Email or username already in use?"
+  //     );
+  //   }
+
+  //   // TODO: this should route back to index page with note to login
+  //   const doLoginToMain = () => {
+  //     navigate("/signin");
+  //   };
 
   return (
     <div className="container-view login-outer-container">
@@ -146,14 +149,34 @@ function RegisterView() {
                   <label htmlFor="firstname" id="label-L">
                     First name:
                   </label>
-                  <input type="text" name="firstname" id="firstname" />
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    title="Enter your first name"
+                    placeholder="Enter your first name"
+                    className="form-control"
+                    onChange={handleChange} // the function we just made! the onChange attribute will automatically pass the `event` argument based off of which input was clicked
+                    value={formData.firstName} // we can use our useState hook to store the values of each input now
+                    required
+                  />
                 </div>
 
                 <div id="box-R" className="reg-name-box">
                   <label htmlFor="lastname" id="label-R">
                     Last name:
                   </label>
-                  <input type="text" name="lastname" id="lastname" />
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    title="Enter your last name"
+                    placeholder="Enter your last name"
+                    className="form-control"
+                    onChange={handleChange}
+                    value={formData.lastName}
+                    required
+                  />
                 </div>
               </div>
 
@@ -164,14 +187,34 @@ function RegisterView() {
                     *must be unregistered valid email
                   </span>
                 </label>
-                <input type="text" name="email" id="email" />
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  title="Enter your email"
+                  placeholder="Enter your last email"
+                  className="form-control"
+                  onChange={handleChange}
+                  value={formData.email}
+                  required
+                />
               </div>
 
               <div className="reg-field-box">
                 <label htmlFor="username">
                   Username: <span className="detail-text">*must be unique</span>
                 </label>
-                <input type="text" name="username" id="username" />
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  title="Create username: "
+                  placeholder="Create username: "
+                  className="form-control"
+                  onChange={handleChange} // the function we just made! the onChange attribute will automatically pass the `event` argument based off of which input was clicked
+                  value={formData.username} // we can use our useState hook to store the values of each input now
+                  required
+                />
               </div>
 
               <div className="reg-field-box">
@@ -181,6 +224,12 @@ function RegisterView() {
                   name="password1"
                   id="password1"
                   className="password-box"
+                  title="Create password"
+                  placeholder="Create password"
+                  //   className="form-control"
+                  onChange={handleChange} // the function we just made! the onChange attribute will automatically pass the `event` argument based off of which input was clicked
+                  value={formData.password1} // we can use our useState hook to store the values of each input now
+                  required
                 />
               </div>
 
@@ -191,6 +240,12 @@ function RegisterView() {
                   name="password2"
                   id="password2"
                   className="password-box"
+                  title="Confirm password"
+                  placeholder="Confirm password"
+                  //   className="form-control"
+                  onChange={handleChange} // the function we just made! the onChange attribute will automatically pass the `event` argument based off of which input was clicked
+                  value={formData.password2} // we can use our useState hook to store the values of each input now
+                  required
                 />
               </div>
 
