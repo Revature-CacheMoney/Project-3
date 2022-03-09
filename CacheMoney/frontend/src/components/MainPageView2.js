43a043067b43416d22ece380b98f29b9d735faaf
unknown <jasongraves96@gmail.com>
2022-03-09 10:40:45 -0500

    made the navbar a component

--- CacheMoney/frontend/src/components/MainPageView2.js
+++ CacheMoney/frontend/src/components/MainPageView2.js
@@ -3,6 +3,7 @@ import Navigation from "./NavBar.jsx";
 import Footer from "./Footer.js";
 import AccountList from "./Account/AccountList.js";
 import userStore from "../store/Store.js";
+import NavBar from "./NavBar.js";
 
 function MainPageView2() {
 	console.log("Data store: ", userStore.getState());
@@ -20,22 +21,7 @@ function MainPageView2() {
 			</div>
 
 			{/* <Navigation /> */}
-			<div className="navigation-bar">
-				<a href="/accounts">
-					<span className="navigation-link">Accounts</span>
-				</a>
-				<a href="/accounts/create">
-					<span className="navigation-link">Create Account</span>
-				</a>
-				<a href="/transfer">
-					<span className="navigation-link">Transfer Money</span>
-				</a>
-				<div className="settings">
-					<a href="#">
-						<span className="navigation-link">Settings</span>
-					</a>
-				</div>
-			</div>
+			<NavBar />
 
 			<div className="main-page-content">
 				{/******* Insert body content here ********/}
