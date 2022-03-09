98b9591c685c4cc90131281ee3db8b33e682e461
Donald Farrar <donfar09@gmail.com>
2022-03-09 14:46:32 -0800

    The registerView page is working properly with the code clean up

--- CacheMoney/frontend/src/components/RegisterView.js
+++ CacheMoney/frontend/src/components/RegisterView.js
@@ -216,7 +216,7 @@ function RegisterView() {
                 />
               </div>
 
-              <input type="submit" value="Register" onClick={handleChange} />
+              <input type="submit" value="Register" onClick={handleSubmit} />
             </div>
           </div>
         </div>
