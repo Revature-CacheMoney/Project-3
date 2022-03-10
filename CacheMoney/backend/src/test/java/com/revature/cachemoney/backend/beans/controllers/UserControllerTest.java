7a0e83767fe3d16befcd62102bf497fa5c3b43e9
DavidAlvaradoSoftware <dalvaradosoftware@gmail.com>
2022-03-10 16:28:31 -0600

    added user controller test

---
+++ CacheMoney/backend/src/test/java/com/revature/cachemoney/backend/beans/controllers/UserControllerTest.java
@@ -0,0 +1,52 @@
+package com.revature.cachemoney.backend.beans.controllers;
+
+import com.revature.cachemoney.backend.beans.services.UsersService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @MockBean
+    private UsersService usersService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void createMockMvc(){
+        assertNotNull(mockMvc);
+    }
+    @Test
+    void getAllUsers() {
+//        assertNotNull(mockMvc);
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void postUser() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getAccountsByUserId() {
+    }
+}
