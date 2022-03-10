04eb8ddc74ca331150341d4937003d6c1b65be81
DavidAlvaradoSoftware <dalvaradosoftware@gmail.com>
2022-03-10 16:30:30 -0600

    changes updated

--- CacheMoney/backend/src/test/java/com/revature/cachemoney/backend/beans/controllers/UserControllerTest.java
+++ CacheMoney/backend/src/test/java/com/revature/cachemoney/backend/beans/controllers/UserControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 
