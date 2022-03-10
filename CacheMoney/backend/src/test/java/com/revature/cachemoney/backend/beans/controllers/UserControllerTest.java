2221d65da79a228b5f1550c0465e08dd11645703
brigardner <97481836+brigardner@users.noreply.github.com>
2022-03-10 17:51:01 -0500

    chcecking out other testing

--- CacheMoney/backend/src/test/java/com/revature/cachemoney/backend/beans/controllers/UserControllerTest.java
+++ CacheMoney/backend/src/test/java/com/revature/cachemoney/backend/beans/controllers/UserControllerTest.java
@@ -1,14 +1,20 @@
 package com.revature.cachemoney.backend.beans.controllers;
 
+import com.revature.cachemoney.backend.beans.models.User;
 import com.revature.cachemoney.backend.beans.services.UsersService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.xml.ws.Response;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.mockito.Mockito.when;
 
 
 @WebMvcTest(UserController.class)
@@ -16,6 +22,8 @@ class UserControllerTest {
 
     @MockBean
     private UsersService usersService;
+    @Autowired
+    private UserController userController;
 
     @Autowired
     private MockMvc mockMvc;
@@ -32,6 +40,10 @@ class UserControllerTest {
 
     @Test
     void getUserById() {
+        Integer testUserId = 1;
+        User testUser = new User("Hank", "Hill", "hank.hill@gmail.com", "abcd1234", "hankhill");
+        when(usersService.getUserById(1)).thenReturn(Optional.of(testUser));
+
     }
 
     @Test
