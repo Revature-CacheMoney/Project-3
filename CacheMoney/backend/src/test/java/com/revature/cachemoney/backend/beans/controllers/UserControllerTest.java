f68d0f289267277f1dbd5ed64e3718e26e8c1016
DavidAlvaradoSoftware <dalvaradosoftware@gmail.com>
2022-03-11 13:01:41 -0600

    User controller testing mostly done, need to find a way to invoke method through endpoint instead of instance.

--- CacheMoney/backend/src/test/java/com/revature/cachemoney/backend/beans/controllers/UserControllerTest.java
+++ CacheMoney/backend/src/test/java/com/revature/cachemoney/backend/beans/controllers/UserControllerTest.java
@@ -2,9 +2,13 @@ package com.revature.cachemoney.backend.beans.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.cachemoney.backend.beans.models.Account;
 import com.revature.cachemoney.backend.beans.models.User;
 import com.revature.cachemoney.backend.beans.security.JwtUtil;
 import com.revature.cachemoney.backend.beans.services.UsersService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,8 +19,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import javax.xml.ws.Response;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,67 +44,124 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private Integer[] testUserIds;
+    private User[] testUsers;
+    private String[] testTokens;
+    private User[] loginUsers;
+    private List<Account> accountList;
 
 
+    @BeforeEach
+    void populateData(){
+        testUserIds = new Integer[2];
+        testUserIds[0] = 1;
+        testUserIds[1] = 2;
 
-//    @Test
-//    void createMockMvc(){
-//        assertNotNull(mockMvc);
-//    }
+        testUsers = new User[2];
+        testUsers[0] = new User("Hank", "Hill", "hank.hill@gmail.com", "abcd1234", "hankhill");
+        testUsers[1] = new User("Peggy", "Hill", "peggy.hill@gmail.com", "efgh5678", "peggyhill");
+
+        testTokens = new String[2];
+        testTokens[0] = "hankToken";
+        testTokens[1] = "peggyToken";
+
+        loginUsers = new User[3];
+        loginUsers[0] = new User(null, null, null, "abcd1234", "hankhill");
+        loginUsers[1] = new User(null, null, null, "efgh5678", "peggy123"); //invalid username (for peggy in testUsers[1]
+        loginUsers[2] = new User(null, null, null, "peggy", "hankhill"); // invalid password for hank in testUsers[0]
+
+        accountList = new LinkedList<>();
+        accountList.add(new Account("checking"));
+        accountList.add(new Account("savings", "Rainy day fund"));
+    }
+    @AfterEach
+    void depopulateData(){
+        testUserIds = null;
+        testUsers = null;
+        testTokens = null;
+        accountList = null;
+        
+    }
+
     @Test
     void getAllUsers() {
-//        assertNotNull(mockMvc);
+
     }
 
     @Test
     void getUserById() throws JsonProcessingException {
-        Integer[] testUserIds = new Integer[2];
-        testUserIds[0] = 1;
-        testUserIds[1] = 2;
-
-        User[] testUsers = new User[2];
-        testUsers[0] = new User("Hank", "Hill", "hank.hill@gmail.com", "abcd1234", "hankhill");
-        testUsers[1] = new User("Peggy", "Hill", "peggy.hill@gmail.com", "efgh5678", "peggyhill");
-
-        String[] testTokens = new String[2];
-        testTokens[0] = "hankToken";
-        testTokens[1] = "peggyToken";
 
         when(usersService.getUserById(testUserIds[0])).thenReturn(Optional.of(testUsers[0]));
         when(usersService.getUserById(testUserIds[1])).thenReturn(Optional.of(testUsers[1]));
         when(jwtUtil.generateToken(testUserIds[0])).thenReturn(testTokens[0]);
         when(jwtUtil.generateToken(testUserIds[1])).thenReturn(testTokens[1]);
 
-        List<ResponseEntity<String>> testResponses = new ArrayList<>();
-        testResponses.add(userController.getUserById(testTokens[0], testUserIds[0]));
-        testResponses.add(userController.getUserById(testTokens[0], testUserIds[1]));
-        testResponses.add(userController.getUserById(testTokens[1], testUserIds[0]));
+        List<ResponseEntity<String>> actualResponses = new ArrayList<>();
+        actualResponses.add(userController.getUserById(testTokens[0], testUserIds[0]));
+        actualResponses.add(userController.getUserById(testTokens[0], testUserIds[1]));
+        actualResponses.add(userController.getUserById(testTokens[1], testUserIds[0]));
 
         List<String> testUsersJson = new ArrayList<>();
         testUsersJson.add(mapper.writeValueAsString(testUsers[0]));
         testUsersJson.add(mapper.writeValueAsString(testUsers[1]));
 
-        assertEquals(HttpStatus.OK, testResponses.get(0).getStatusCode());
-        assertEquals(testUsersJson.get(0), testResponses.get(0).getBody());
+        assertEquals(HttpStatus.OK, actualResponses.get(0).getStatusCode());
+        assertEquals(testUsersJson.get(0), actualResponses.get(0).getBody());
 
 //        assertEquals(HttpStatus.BAD_REQUEST, testResponses.get(1).getStatusCode()); // Http status should not be OK - AspectJwt not being reached
 //        assertNotEquals(testUsersJson.get(0), testResponses.get(1).getBody());
 //        assertNotEquals(testUsersJson.get(1), testResponses.get(1).getBody());
     }
 
-//    @Test
-//    void postUser() {
-//    }
 //
-//    @Test
-//    void deleteUserById() {
-//    }
+    @Test
+    void deleteUserById() {
+    }
 //
-//    @Test
-//    void login() {
-//    }
+    @Test
+    void login() throws JsonProcessingException {
+
+        testUsers[0].setUser_id(1);
+        when(usersService.getUserByUsername(loginUsers[0])).thenReturn(testUsers[0]);
+        when(jwtUtil.generateToken(testUsers[0].getUser_id())).thenReturn(testTokens[0]);
+        ResponseEntity<String> response = userController.login(loginUsers[0]);
+
+        assertEquals(mapper.writeValueAsString(testUsers[0]), response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        //to retrieve a specific value from a header, use ,getFirst(your-header-name)
+        assertEquals(testTokens[0], response.getHeaders().getFirst("JWT"));
+
+        // incorrect username
+        when(usersService.getUserByUsername(loginUsers[1])).thenReturn(null);
+        ResponseEntity<String> response1 = userController.login(loginUsers[1]);
+        assertEquals(HttpStatus.BAD_REQUEST, response1.getStatusCode());
+
+        // incorrect password
+        when(usersService.getUserByUsername(loginUsers[2])).thenReturn(null);
+        ResponseEntity<String> response2 = userController.login(loginUsers[2]);
+        assertEquals(HttpStatus.BAD_REQUEST, response2.getStatusCode());
+
+
+
+    }
 //
-//    @Test
-//    void getAccountsByUserId() {
-//    }
+    @Test
+    void getAccountsByUserId() throws JsonProcessingException {
+        //set user id to 1
+        testUsers[0].setUser_id(1);
+        when(usersService.getAccountsByUserId(testUsers[0].getUser_id())).thenReturn(accountList);
+        ResponseEntity<String> response = userController.getAccountsByUserId(testTokens[0], 1);
+        assertEquals(mapper.writeValueAsString(accountList), response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+
+    @Test
+    void postUser() {
+        when(usersService.postUser(testUsers[0])).thenReturn(true);
+        assertTrue(userController.postUser(testUsers[0]));
+        when(usersService.postUser(loginUsers[0])).thenReturn(false);
+        assertFalse(userController.postUser(loginUsers[0]));
+    }
+
 }
