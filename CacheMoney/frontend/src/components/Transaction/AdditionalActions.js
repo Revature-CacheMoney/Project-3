5541bf0708b960d0e74cb530fa90329c2ba2aaf2
Cheryl See <cheryl.see@revature.net>
2022-03-14 14:20:57 -0500

    Tweaking account list to position better

--- CacheMoney/frontend/src/components/Transaction/AdditionalActions.js
+++ CacheMoney/frontend/src/components/Transaction/AdditionalActions.js
@@ -1,16 +1,53 @@
 import React from "react";
+import Deposit from "./Deposit.js";
+import Withdraw from "./Withdraw.js";
+import Transfer from "./Transfer.js";
 
 function AdditionalActions() {
-	const additionalContent = () => {};
+	let additionalContent = (type) => {
+		switch (type) {
+			case "deposit":
+				return <Deposit />;
+			case "withdraw":
+				return <Withdraw />;
+			case "transfer":
+				return <Transfer />;
+			default:
+				return;
+		}
+	};
+
+	const handleOptionSelection = (event) => {
+		console.log(event.target.value);
+		additionalContent(event.target.value);
+	};
 
 	return (
-		<div class="account-details">
-			<div class="account-additional-options">
-				<button class="account-option">Deposit</button>
-				<button class="account-option">Withdrawal</button>
-				<button class="account-option">Transfer</button>
+		<div className="account-details">
+			<div className="account-additional-options">
+				<button
+					class="account-option"
+					value="deposit"
+					onClick={handleOptionSelection}
+				>
+					Deposit
+				</button>
+				<button
+					className="account-option"
+					value="withdraw"
+					onClick={handleOptionSelection}
+				>
+					Withdraw
+				</button>
+				<button
+					className="account-option"
+					value="transfer"
+					onClick={handleOptionSelection}
+				>
+					Transfer
+				</button>
 			</div>
-			<div class="additional-options-content"></div>
+			<div className="additional-options-content">{additionalContent}</div>
 		</div>
 	);
 }
