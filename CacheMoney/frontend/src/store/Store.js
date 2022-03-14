ed8df74d30c2450909d616e41e3c6f4b972e3481
jondgons <jondgons@gmail.com>
2022-03-14 10:40:10 -0500

    transfer reducer

--- CacheMoney/frontend/src/store/Store.js
+++ CacheMoney/frontend/src/store/Store.js
@@ -5,10 +5,11 @@
 import { combineReducers, createStore } from 'redux';
 import userReducer from './reducers/UserReducer';
 import accountReducer from './reducers/AccountReducer';
+import transferReducer from './reducers/TransferReducer';
 
 
 // const userStore = createStore(userReducer);
 // const accountStore = createStore(accountReducer);
-const store = createStore(combineReducers({ userReducer, accountReducer }));
+const store = createStore(combineReducers({ userReducer, accountReducer, transferReducer }));
 
 export default store;
