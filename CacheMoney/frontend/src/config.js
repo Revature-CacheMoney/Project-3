eaee4540837edd3ec070c9436c30d81bf672f94d
jondgons <jondgons@gmail.com>
2022-03-16 10:44:50 -0500

    frontend urls

--- CacheMoney/frontend/src/config.js
+++ CacheMoney/frontend/src/config.js
@@ -1,5 +1,7 @@
 const config = {
     url: "http://cachemoney-env.eba-vtpiesgf.us-east-1.elasticbeanstalk.com/"
+    // local: "http://localhost:8080/"
+    // remote: "http://cachemoney-env.eba-vtpiesgf.us-east-1.elasticbeanstalk.com/"
 };
 
 module.exports = config;
