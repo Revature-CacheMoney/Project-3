81e574023fc9f47800df0400bdae0304ef91b32d
Cheryl See <cheryl.see@revature.net>
2022-03-16 17:23:53 -0500

    Accounts should theoretically update now

--- CacheMoney/frontend/src/components/Transaction/Withdraw.js
+++ CacheMoney/frontend/src/components/Transaction/Withdraw.js
@@ -6,53 +6,61 @@ import config from "../../config";
 import store from "../../store/Store";
 
 function Withdraw(props) {
-    // post withdraw transaction
-    const postWithdraw = (transaction) => {
-        axios.post(`${config.url}accounts/withdraw`, transaction,
-            {
-                headers: {
-                    token: store.getState().userReducer.token,
-                    userId: store.getState().userReducer.userId
-                }
-            })
-            .catch(error => console.error(`Error: ${error}`));
-    }
+	// post withdraw transaction
+	const postWithdraw = (transaction) => {
+		axios
+			.post(`${config.url}accounts/withdraw`, transaction, {
+				headers: {
+					token: store.getState().userReducer.token,
+					userId: store.getState().userReducer.userId,
+				},
+			})
+			.catch((error) => console.error(`Error: ${error}`));
+	};
 
-    // what the submit button should do
-    const handleSubmit = (event) => {
-        // prevent page reloading
-        event.preventDefault();
+	// what the submit button should do
+	const handleSubmit = (event) => {
+		// prevent page reloading
+		event.preventDefault();
 
-        // create the withdraw payload
-        let withdraw = {
-            account: {
-                accountId: store.getState().accountReducer.currentAccountId
-            },
-            transactionAmount: event.target.transactionAmount.value
-        }
+		// create the withdraw payload
+		let withdraw = {
+			account: {
+				accountId: store.getState().accountReducer.currentAccountId,
+			},
+			transactionAmount: event.target.transactionAmount.value,
+		};
 
-        // perform the post
-        postWithdraw(withdraw);
-    }
+		// perform the post
+		postWithdraw(withdraw);
+		// maybe do this in/after the api call
+		props.doTransactionDone(Date.now());
+	};
 
-    return (
-        <div className="withdraw-outer-container">
-            <div className="withdraw-inner-container">
-                <div className="withdraw-form">
-                    <p className="withdraw-form-header">Withdraw</p>
+	return (
+		<div className="withdraw-outer-container">
+			<div className="withdraw-inner-container">
+				<div className="withdraw-form">
+					<p className="withdraw-form-header">Withdraw</p>
 
-                    <form onSubmit={handleSubmit}>
-                        <div className="withdraw-amount">
-                            <label>Amount</label>
-                            <input type="number" min="0.01" step="0.01" id="withdraw-input" name="transactionAmount" />
-                        </div>
+					<form onSubmit={handleSubmit}>
+						<div className="withdraw-amount">
+							<label>Amount</label>
+							<input
+								type="number"
+								min="0.01"
+								step="0.01"
+								id="withdraw-input"
+								name="transactionAmount"
+							/>
+						</div>
 
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+						<button type="submit">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Withdraw;
