c6ba957278bda0a4748b85dae132d261fbf6e9d4
jondgons <jondgons@gmail.com>
2022-03-16 19:37:38 -0500

    cursed solution for images on remote hosting

--- CacheMoney/frontend/src/components/style/Themes.js
+++ CacheMoney/frontend/src/components/style/Themes.js
@@ -1,7 +1,8 @@
-import dark from "../../image/Dark_Background_image-01.png"
-import light from "../../image/CacheMoney_MoneyBackground_Smaller.png"
-import logo from "../../image/CacheMoney-logo.png"
-import logoDark from "../../image/CacheMoney-logo-dark.png"
+// import dark from "../../image/Dark_Background_image-01.png"
+// import light from "../../image/CacheMoney_MoneyBackground_Smaller.png"
+// import logo from "../../image/CacheMoney-logo.png"
+// import logoDark from "../../image/CacheMoney-logo-dark.png"
+import config from "../../config"
 
 export const lightTheme = {
     gentle: "#6c896a",
@@ -15,9 +16,9 @@ export const lightTheme = {
     greyness: "#4c4c4b",
     specialButtons: "#d3a940",
     error: "#cc3040",
-    splashBackground: "url(http://localhost:3000/static/media/Background_image-01.7bea5d27b65a2d0c5b9a.png)",
-    moneyBackground: "url(http://localhost:3000/static/media/CacheMoney_MoneyBackground_Smaller.94ddae7aa0898659895e.png)",
-    cacheMoney: "url(http://localhost:3000/static/media/CacheMoney-logo.e3731fba85899852b417.png)",
+    splashBackground: `url(${config.url_img}static/media/Background_image-01.7bea5d27b65a2d0c5b9a.png)`,
+    moneyBackground: `url(${config.url_img}static/media/CacheMoney_MoneyBackground_Smaller.94ddae7aa0898659895e.png)`,
+    cacheMoney: `url(${config.url_img}static/media/CacheMoney-logo.e3731fba85899852b417.png)`,
 }
 
 export const darkTheme = {
@@ -32,7 +33,7 @@ export const darkTheme = {
     specialButtons: "#3e4f3d",
     greyness: "#4c4c4b",
     error: "#cc3040",
-    splashBackground: "url(http://localhost:3000/static/media/Dark_Background_image-01.fa8d53963c1bb0c6ce45.png)",
-    moneyBackground: "url(http://localhost:3000/static/media/CacheMoney_MoneyBackground_Dark.904d1499ea29d61935c7.png)",
-    cacheMoney: "url(http://localhost:3000/static/media/CacheMoney-logo-dark.533bbb747a4eca262de6.png)",
+    splashBackground: `url(${config.url_img}static/media/Dark_Background_image-01.fa8d53963c1bb0c6ce45.png)`,
+    moneyBackground: `url(${config.url_img}static/media/CacheMoney_MoneyBackground_Dark.904d1499ea29d61935c7.png)`,
+    cacheMoney: `url(${config.url_img}static/media/CacheMoney-logo-dark.533bbb747a4eca262de6.png)`,
 }
--- CacheMoney/frontend/src/config.js
+++ CacheMoney/frontend/src/config.js
@@ -1,7 +1,11 @@
 const config = {
-    url: "http://cachemoney-env.eba-vtpiesgf.us-east-1.elasticbeanstalk.com/"
+    url: "http://cachemoney-env.eba-vtpiesgf.us-east-1.elasticbeanstalk.com/",
     // local: "http://localhost:8080/"
     // remote: "http://cachemoney-env.eba-vtpiesgf.us-east-1.elasticbeanstalk.com/"
+    
+    url_img: "http://revature-cachemoney.s3-website-us-east-1.amazonaws.com/"
+    // local: "http://localhost:3000/"
+    // remote: "http://revature-cachemoney.s3-website-us-east-1.amazonaws.com/"
 };
 
-module.exports = config;
+export default config;
