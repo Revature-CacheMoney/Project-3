a51addfd7f8d7d4760eab7657ae00fb00511a4f8
Ethan Edmond <eedmond@uccs.edu>
2022-04-06 15:04:57 -0600

    Added options to select in Request

--- CacheMoney/frontend/src/components/SendRequest/Request.js
+++ CacheMoney/frontend/src/components/SendRequest/Request.js
@@ -1,14 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import config from "../../config";
+import store from "../../store/Store";
 import "./Request.css";
 
 const Request = (props) => {
+    const [accounts, setAccounts] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${config.url}users/accounts`, {
+            headers: {
+                token: store.getState().userReducer.token,
+                userId: store.getState().userReducer.userId,
+            }})
+        .then((response) => {
+            setAccounts(response.data);
+        })
+    },[]);
+
+    console.log(accounts);
+
+    const options = accounts.map(account => {
+        return (
+            <option key={account.accountId} value={account.accountId}>{account.name} (id: {account.accountId})</option>
+        );
+    });
     return (
         <form>
             <label htmlFor="source-id">Source Account</label>
             <input id="source-id" type="number"/>
             
             <label htmlFor="destination-id">Destination Account</label>
-            <input id="destination-id" type="number"/>
+            <select id="destination-id">
+                {options}
+            </select>
 
             <label htmlFor="amount">Amount of Money</label>
             <input id="amount" type="number" min={0.01} step={0.01}/>
