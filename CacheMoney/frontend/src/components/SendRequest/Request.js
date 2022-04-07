d62d9663e7388f0ba2bf532121da8be2cd25c9ff
Sfierros <sfierros12@gmail.com>
2022-04-07 15:19:55 -0400

    functional request so far

--- CacheMoney/frontend/src/components/SendRequest/Request.js
+++ CacheMoney/frontend/src/components/SendRequest/Request.js
@@ -7,6 +7,14 @@ import "./Request.css";
 const Request = (props) => {
     const [accounts, setAccounts] = useState([]);
 
+    //cutting a slice of state to hold form data
+    const  [formData, setFormData]  = useState({
+        sourceAccountId: 0,
+        destinationAccountId: "",
+        description: "",
+        amount: 0,
+    });
+
     useEffect(() => {
         axios.get(`${config.url}users/accounts`, {
             headers: {
@@ -15,31 +23,52 @@ const Request = (props) => {
             }})
         .then((response) => {
             setAccounts(response.data);
+            setFormData({...formData, destinationAccountId: response.data[0].accountId})
         })
     },[]);
-
-    console.log(accounts);
 
     const options = accounts.map(account => {
         return (
             <option key={account.accountId} value={account.accountId}>{account.name} (id: {account.accountId})</option>
         );
     });
+    
+    
+
+    //setting formData using setFormData to update dynamically
+    const handleChange = (event) => {
+        let value = event.target.value;
+        if(event.target.name == "sourceAccountId" || event.target.name == "amount"){
+            value = parseInt(value, 10);
+        }
+        setFormData({...formData, [event.target.name]: value});
+    }
+
+    //handles submit request
+    const handleSubmit = (submitRequest) => {
+        submitRequest.preventDefault();
+        console.log(formData);
+    }
+    
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="source-id">Source Account</label>
-            <input id="source-id" type="number"/>
+            <input id="source-id" type="number" min={0} step={1} name="sourceAccountId" onChange={handleChange}/>
+
             
             <label htmlFor="destination-id">Destination Account</label>
-            <select id="destination-id">
+            <select value={formData.destinationAccountId} id="destination-id" name="destinationAccountId" onChange={handleChange}>
+                {accounts.length == 0 && <option value={null}>No Accounts to be Displayed</option>}
                 {options}
             </select>
 
             <label htmlFor="amount">Amount of Money</label>
-            <input id="amount" type="number" min={0.01} step={0.01}/>
+            <input id="amount" type="number" min={0.01} step={0.01} name="amount" onChange={handleChange}/>
 
             <label htmlFor="description">Memo/Description</label>
-            <input id="description" type="text" maxLength={255}/>
+            <input id="description" type="text" maxLength={255} name="description" onChange={handleChange}/>
+
+            <button id="submit-request"> Request Money </button>
         </form>
     );
 }
