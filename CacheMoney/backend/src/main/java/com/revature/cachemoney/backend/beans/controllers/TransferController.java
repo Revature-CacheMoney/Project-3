93630c344ff307d489d4746e6da4e75a9770cec0
Ethan Edmond <eedmond@uccs.edu>
2022-04-08 10:19:53 -0600

    Trying a fix for the jwt stuff

--- CacheMoney/backend/src/main/java/com/revature/cachemoney/backend/beans/controllers/TransferController.java
+++ CacheMoney/backend/src/main/java/com/revature/cachemoney/backend/beans/controllers/TransferController.java
@@ -2,8 +2,10 @@ package com.revature.cachemoney.backend.beans.controllers;
 
 import com.revature.cachemoney.backend.beans.annotations.RequireJwt;
 import com.revature.cachemoney.backend.beans.models.Transfer;
+import com.revature.cachemoney.backend.beans.security.JwtUtil;
 import com.revature.cachemoney.backend.beans.services.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -13,33 +15,47 @@ import java.util.List;
 @RequestMapping("transfer")
 public class TransferController {
     private TransferService transferService;
+    private JwtUtil jwtUtil;
 
     @Autowired
-    public TransferController(TransferService transferService) {
+    public TransferController(TransferService transferService, JwtUtil jwtUtil) {
         this.transferService = transferService;
+        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping(value = "destination")
-    @RequireJwt
+    // TODO @RequireJwt
     public List<Transfer> findByDestinationUser(
             @RequestHeader(name = "token") String token,
-            @RequestHeader(name = "userId") Integer userId) {
-        return this.transferService.findByDestinationUser(userId);
+            @RequestHeader(name = "userId") Integer userId) throws ResponseStatusException {
+        if (this.jwtUtil.validateToken(token, userId)) {
+            return this.transferService.findByDestinationUser(userId);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Please provide a valid token");
+        }
     }
 
     @GetMapping(value = "source")
-    @RequireJwt
+    // TODO @RequireJwt
     public List<Transfer> findBySourceUser(
             @RequestHeader(name = "token") String token,
-            @RequestHeader(name = "userId") Integer userId) {
-        return this.transferService.findBySourceUser(userId);
+            @RequestHeader(name = "userId") Integer userId) throws ResponseStatusException {
+        if (this.jwtUtil.validateToken(token, userId)) {
+            return this.transferService.findBySourceUser(userId);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Please provide a valid token");
+        }
     }
 
     @PostMapping
-    @RequireJwt
+    // TODO @RequireJwt
     public Transfer saveTransfer(@RequestBody Transfer transfer,
                                  @RequestHeader(name = "token") String token,
                                  @RequestHeader(name = "userId") Integer userId) throws ResponseStatusException {
-        return this.transferService.save(transfer, userId);
+        if (this.jwtUtil.validateToken(token, userId)) {
+            return this.transferService.save(transfer, userId);
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Please provide a valid token");
+        }
     }
 }
